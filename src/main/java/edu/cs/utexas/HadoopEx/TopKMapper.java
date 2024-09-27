928282e2d798edc33a163530e44a001e29dbe8f8
Abdomash <abdomash2010@gmail.com>
2024-09-27 05:02:59 -0500

    fix code and update results

--- src/main/java/edu/cs/utexas/HadoopEx/TopKMapper.java
+++ src/main/java/edu/cs/utexas/HadoopEx/TopKMapper.java
@@ -9,13 +9,14 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import edu.cs.utexas.HadoopEx.TopKTaxiMap.Entry;
-
 public class TopKMapper extends Mapper<Object, Text, Text, FloatWritable> {
-	private static final Logger LOGGER = LogManager.getLogger(TopKMapper.class.getName());
 
 	private static final int K = 10;
-	private TopKTaxiMap topK = new TopKTaxiMap(K);
+	private PriorityQueue<Entry> topK;
+
+	public TopKMapper() {
+		topK = new PriorityQueue<Entry>(K);
+	}
 
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 		String[] columns = value.toString().split("\t");
@@ -23,15 +24,19 @@ public class TopKMapper extends Mapper<Object, Text, Text, FloatWritable> {
 		Float amount = Float.parseFloat(columns[1]);
 		Float duration = Float.parseFloat(columns[2]);
 		float earning = amount / duration;
-		topK.add(taxiID, earning);
+		topK.add(new Entry(new Text(taxiID), new FloatWritable(earning)));
+		while (topK.size() > K) {
+			topK.poll();
+		}
+		// Entry entry = new Entry(new Text(taxiID), new FloatWritable(earning));
+		// context.write(entry.getKey(), entry.getValue());
 	}
 
 	@Override
 	public void cleanup(Context context) throws IOException, InterruptedException {
-		for (Entry entry : topK.getTopK()) {
-			Text taxiID = new Text(entry.getKey());
-			FloatWritable earning = new FloatWritable(entry.getValue());
-			context.write(taxiID, earning);
+		while (!topK.isEmpty()) {
+			Entry entry = topK.poll();
+			context.write(entry.getKey(), entry.getValue());
 		}
 	}
 }
--- src/main/java/edu/cs/utexas/HadoopEx/TopKReducer.java
+++ src/main/java/edu/cs/utexas/HadoopEx/TopKReducer.java
@@ -4,30 +4,31 @@ import java.io.IOException;
 import java.util.PriorityQueue;
 
 import org.apache.hadoop.io.FloatWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import edu.cs.utexas.HadoopEx.TopKTaxiMap.Entry;
-
-public class TopKReducer extends  Reducer<Text, Text, Text, FloatWritable> {
-     private static final Logger LOGGER = LogManager.getLogger(TaxiErrorReducer.class.getName());
-     
+public class TopKReducer extends  Reducer<Text, Text, Text, FloatWritable> {     
      private static final int K = 10;
-     private TopKTaxiMap topK = new TopKTaxiMap(K);
+     private PriorityQueue<Entry> topK;
+
+     public TopKReducer() {
+          topK = new PriorityQueue<Entry>(K);
+     }
 
      public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException {
-
-          for (FloatWritable value : values) {
-               topK.add(key.toString(), value.get());
+          for (FloatWritable value : values) { // values is always a single element
+               topK.add(new Entry(new Text(key), new FloatWritable(value.get())));
+               if (topK.size() > K) {
+                    topK.poll();
+               }
           }
+     }
 
-          for (Entry entry : topK.getTopK()) {
-               Text taxiID = new Text(entry.getKey());
-               FloatWritable errorRate = new FloatWritable(entry.getValue());
-               context.write(taxiID, errorRate);
+     @Override
+     public void cleanup(Context context) throws IOException, InterruptedException {
+          while (!topK.isEmpty()) {
+               Entry entry = topK.poll();
+               context.write(entry.getKey(), entry.getValue());
           }
      }
 }
